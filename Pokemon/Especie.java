fa3e5c74bb67f067d34ac0f516826371a3d62989
hrq-araujo <37453824+hrq-araujo@users.noreply.github.com>
2020-10-17 22:20:12 -0300

    Update Especie.java
    em busca da peita perdida

--- Pokemon/Especie.java
+++ Pokemon/Especie.java
@@ -11,6 +11,7 @@ public class Especie {
     private double baseDef;
     private double baseSpe;
     private double baseSpd;
+    private double olhaSo;
 
     public Especie(String[][] tabelaEspecie, int i){
         this.id = Integer.parseInt(tabelaEspecie[i][0]);
@@ -23,6 +24,7 @@ public class Especie {
         this.baseDef = Integer.parseInt(tabelaEspecie[i][6]);
         this.baseSpe = Integer.parseInt(tabelaEspecie[i][7]);
         this.baseSpd = Integer.parseInt(tabelaEspecie[i][8]);
+        this.olhaSo = 69;
     }
 
     public double calcularHP(int level){
